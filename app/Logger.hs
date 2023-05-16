1c47c1f0c31f2b5c7f37927a482b8798ee7224e1
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-05-16 21:46:13 +0200

    Add simple Logger Effect

---
+++ app/Logger.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- | Very simple effect for logging
+module Logger where
+
+import Control.Monad.IO.Class (liftIO)
+import Data.Text (Text)
+import Data.Text.IO qualified as Text
+import Display (Display)
+import Display qualified
+import Effectful (Eff, Effect, IOE, (:>))
+import Effectful.Dispatch.Dynamic (interpret)
+import Effectful.TH (makeEffect)
+
+data Logger :: Effect where
+  LogInfo :: Text -> Logger m ()
+
+makeEffect ''Logger
+
+-- | Log everything to stdout
+logStdout :: IOE :> es => Eff (Logger : es) a -> Eff es a
+logStdout = interpret $ \_ -> \case
+  LogInfo msg -> liftIO $ Text.putStrLn msg
+
+-- | Helper function to print something that has a Display instance
+displayInfo :: (Display a, Logger :> es) => a -> Eff es ()
+displayInfo a = logInfo $ Display.displayText a
