a7792fa79671edeb54fa8b0e7d2169f2b3e42f08
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-05-21 22:38:59 +0200

    Do not error when directory is missing

--- app/Backup/RSync.hs
+++ app/Backup/RSync.hs
@@ -12,8 +12,11 @@ import Config.Backup.Rsync (RsyncBackupConfig (..))
 import Effectful (Dispatch (Dynamic), DispatchOf, Eff, Effect, IOE, (:>))
 import Effectful.Dispatch.Dynamic (reinterpret)
 import Effectful.Error.Static (Error)
+import Effectful.Error.Static qualified as Error
 import Effectful.FileSystem qualified as FileSystem
 import Effectful.TH (makeEffect)
+import Logger (Logger)
+import Logger qualified
 
 data RSync :: Effect where
   Run :: RsyncBackupConfig -> RSync m ()
@@ -23,18 +26,25 @@ type instance DispatchOf RSync = 'Dynamic
 makeEffect ''RSync
 
 runRSync
-  :: (IOE :> es, Command :> es, Error CommandError :> es)
+  :: (Error.HasCallStack, IOE :> es, Command :> es, Logger :> es, Error CommandError :> es)
   => Eff (RSync : es) a
   -> Eff es a
 runRSync = reinterpret FileSystem.runFileSystem $ \_ -> \case
   Run config -> do
-    FileSystem.createDirectoryIfMissing True config.backupDestination
+    sourceExists <- FileSystem.doesPathExist config.backupSource
+    if not sourceExists
+      then -- TODO: throw an error?
 
-    Command.runProcessThrowOnError
-      "rsync"
-      [ "--archive"
-      , "--fsync"
-      , config.backupSource
-      , config.backupDestination
-      ]
-      ""
+        Logger.displayInfo $
+          "Source location " <> config.backupSource <> " does not exist! Skipping backup!"
+      else do
+        FileSystem.createDirectoryIfMissing True config.backupDestination
+
+        Command.runProcessThrowOnError
+          "rsync"
+          [ "--archive"
+          , "--fsync"
+          , config.backupSource
+          , config.backupDestination
+          ]
+          ""
