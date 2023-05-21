d657d36f08c32119e4fd256e053c792bd050b81c
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-05-21 16:48:20 +0200

    Log external disk steps

--- app/Backup/ExternalDisk.hs
+++ app/Backup/ExternalDisk.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -9,6 +10,9 @@ module Backup.ExternalDisk (ExternalDiskBackup, run, runExternalDiskBackup, loop
 import Backup.RSync (RSync)
 import Backup.RSync qualified as RSync
 import Config.Backup.ExternalDisk (ExternalDiskBackupConfig (..))
+import Config.Backup.Rsync (RsyncBackupConfig (..))
+import Config.Drive (MountDriveConfig (..))
+import Display (Display (..))
 import Drive.MountDrive (MountDrive)
 import Drive.MountDrive qualified as MountDrive
 import Effectful (Eff, Effect, (:>))
@@ -17,6 +21,8 @@ import Effectful.Dispatch.Dynamic (interpret)
 import Effectful.Reader.Static (Reader)
 import Effectful.Reader.Static qualified as Reader
 import Effectful.TH (makeEffect)
+import Logger (Logger)
+import Logger qualified
 
 -- | Waits for the a disk to appear, mounts it, backs up a directory, and then unmounts the disk
 -- again
@@ -25,15 +31,45 @@ data ExternalDiskBackup :: Effect where
 
 makeEffect ''ExternalDiskBackup
 
+-- | Represents the steps taken in the backup, used for logging
+data TraceSteps
+  = WaitingForDisk ExternalDiskBackupConfig
+  | StartingBackup ExternalDiskBackupConfig
+  | BackupComplete ExternalDiskBackupConfig
+
+instance Display TraceSteps where
+  display = \case
+    WaitingForDisk config ->
+      "Backup "
+        <> display config.rsyncConfig.backupName
+        <> ": Waiting for disk "
+        <> display config.mountConfig.driveUuid
+        <> " to appear"
+    StartingBackup config ->
+      "Backup "
+        <> display config.rsyncConfig.backupName
+        <> ": Disk "
+        <> display config.mountConfig.driveUuid
+        <> " is available, starting backup."
+    BackupComplete config ->
+      "Backup "
+        <> display config.rsyncConfig.backupName
+        <> ": backup finished, unmounting disk."
+
 runExternalDiskBackup
-  :: (MountDrive :> es, RSync :> es, Concurrent :> es)
+  :: (MountDrive :> es, RSync :> es, Concurrent :> es, Logger :> es)
   => Eff (ExternalDiskBackup : es) a
   -> Eff es a
 runExternalDiskBackup = interpret $ \_ -> \case
   Run config -> do
     Reader.runReader config.mountConfig $ do
+      Logger.displayTrace $ WaitingForDisk config
       MountDrive.blockUntilDiskAvailable
+
+      Logger.displayTrace $ StartingBackup config
       RSync.run config.rsyncConfig
+      Logger.displayTrace $ BackupComplete config
+
       MountDrive.unmount
 
 -- | Waits for a disk to appear, mounts it, backs up a directory, unmounts the disk again, waits
