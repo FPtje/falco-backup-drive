523227ddceed542cd535a01cda392e20e492d283
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-05-24 21:13:27 +0200

    Add Sqlite effect

---
+++ app/Database/Persistent/SqliteEffect.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Database.Persistent.SqliteEffect where
+
+import Control.Monad.Logger (NoLoggingT)
+import Control.Monad.Logger.CallStack (mapNoLoggingT)
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.Reader qualified as ReaderT
+import Control.Monad.Trans.Resource (ResourceT, transResourceT)
+import Data.Text (Text)
+import Database.Persist.Sqlite qualified as Persist
+import Effectful (Eff, Effect, IOE, (:>))
+import Effectful.Dispatch.Dynamic (interpret, localSeqUnliftIO)
+import Effectful.TH (makeEffect)
+
+-- | An effect that represents a subset of the functions in persistent-sqlite. Persistent is coupled
+-- to mtl-style transformers from mtl, monad-logger, and resourcet. This effect is a middle ground
+-- to using these mtl-style effects, but also still staying within the Eff monad.
+data Sqlite :: Effect where
+  RunSqlite :: Text -> ReaderT Persist.SqlBackend (NoLoggingT (ResourceT m)) a -> Sqlite m a
+
+makeEffect ''Sqlite
+
+runSqliteIO :: IOE :> es => Eff (Sqlite : es) a -> Eff es a
+runSqliteIO = interpret $ \env -> \case
+  RunSqlite connectionString m ->
+    localSeqUnliftIO env $ \unlift ->
+      Persist.runSqlite
+        connectionString
+        (ReaderT.mapReaderT (mapNoLoggingT (transResourceT unlift)) m)
--- nix/haskell-overlay.nix
+++ nix/haskell-overlay.nix
@@ -4,4 +4,6 @@ final: previous:
   falco-backup-drive = final.callPackage ../default.nix {};
 
   conferer = haskell.lib.doJailbreak previous.conferer;
+  postgresql-simple = haskell.lib.doJailbreak previous.postgresql-simple;
+  persistent = haskell.lib.doJailbreak previous.persistent;
 }
