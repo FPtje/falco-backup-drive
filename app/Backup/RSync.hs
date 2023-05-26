deabda6bc323ab825c869533bfb3b75081ccc55e
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-05-26 15:52:39 +0200

    Log on rsync backup

--- app/Backup/RSync.hs
+++ app/Backup/RSync.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -9,13 +10,14 @@ module Backup.RSync where
 import Command (Command, CommandError)
 import Command qualified
 import Config.Backup.Rsync (RsyncBackupConfig (..))
+import Display (Display (..))
 import Effectful (Dispatch (Dynamic), DispatchOf, Eff, Effect, IOE, (:>))
 import Effectful.Dispatch.Dynamic (reinterpret)
 import Effectful.Error.Static (Error)
 import Effectful.Error.Static qualified as Error
 import Effectful.FileSystem qualified as FileSystem
 import Effectful.TH (makeEffect)
-import Logger (Logger)
+import Logger (Logger, displayInfo)
 
 data RSync :: Effect where
   Run :: RsyncBackupConfig -> RSync m ()
@@ -25,13 +27,14 @@ type instance DispatchOf RSync = 'Dynamic
 makeEffect ''RSync
 
 runRSync
-  :: (Error.HasCallStack, IOE :> es, Command :> es, Logger :> es, Error CommandError :> es)
+  :: (Error.HasCallStack, IOE :> es, Command :> es, Logger :> es, Error CommandError :> es, Logger :> es)
   => Eff (RSync : es) a
   -> Eff es a
 runRSync = reinterpret FileSystem.runFileSystem $ \_ -> \case
   Run config -> do
     FileSystem.createDirectoryIfMissing True config.backupDestination
 
+    Logger.displayInfo $ "Starting rsync backup " <> display config.backupName
     Command.runProcessThrowOnError
       "rsync"
       [ "--archive"
@@ -40,3 +43,4 @@ runRSync = reinterpret FileSystem.runFileSystem $ \_ -> \case
       , config.backupDestination
       ]
       ""
+    Logger.displayInfo $ "Finished rsync backup " <> display config.backupName
